b4acdc77ed46213fe404a5f1b7939b7774a8e600
Md Himel Hasan <hmdhimel1234@gmail.com>
2025-12-03 12:32:30 +0600

    Secrete

--- rentalhouse/rentalproject/rentalproject/settings.py
+++ rentalhouse/rentalproject/rentalproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#8s^or7+rgzk+jsr77ismai0pn*0cmp%_-xl&%3de&jdf^_yi1'
+SECRET_KEY = {Django_key}
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
