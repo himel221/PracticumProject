2a7b06f07e5434f1f16fc24066b931465c1e2e7b
Md Himel Hasan <hmdhimel1234@gmail.com>
2025-12-03 12:29:48 +0600

    change Allowed Host

--- rentalhouse/rentalproject/rentalproject/settings.py
+++ rentalhouse/rentalproject/rentalproject/settings.py
@@ -131,7 +131,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+SECRET_KEY = {Django_key}
 DEBUG = True
 ALLOWED_HOSTS = ['*']
 
